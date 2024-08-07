07507ca3168e495dac50cf29b87e0c5e747b5f1f
finallie <1006492957@qq.com>
2024-08-07 16:11:18 +0800

    scanner

--- vm.c
+++ vm.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+#include "compiler.h"
 #include "debug.h"
 
 VM vm;
@@ -70,10 +71,9 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
 void push(Value value) {
