bcc7d4266faaf2b1ee8f6bf373b981ea0e133213
finallie <1006492957@qq.com>
2024-08-26 15:40:09 +0800

    function

--- object.c
+++ object.c
@@ -28,19 +28,33 @@ static ObjString *allocateString(char *chars, int length,
     return string;
 }
 
-static uint32_t hashString(const char* key, int length) {
+static uint32_t hashString(const char *key, int length) {
     uint32_t hash = 2166136261u;
     for (int i = 0; i < length; i++) {
-        hash ^= (uint8_t)key[i];
+        hash ^= (uint8_t) key[i];
         hash *= 16777619;
     }
     return hash;
 }
 
+ObjFunction *newFunction() {
+    ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+    function->arity = 0;
+    function->name = NULL;
+    initChunk(&function->chunk);
+    return function;
+}
+
+ObjNative *newNative(NativeFn function) {
+    ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+    native->function = function;
+    return native;
+}
+
 ObjString *takeString(char *chars, int length) {
     uint32_t hash = hashString(chars, length);
-    ObjString* interned = tableFindString(&vm.strings, chars, length,
-                                        hash);
+    ObjString *interned = tableFindString(&vm.strings, chars, length,
+                                          hash);
     if (interned != NULL) {
         FREE_ARRAY(char, chars, length + 1);
         return interned;
@@ -51,8 +65,8 @@ ObjString *takeString(char *chars, int length) {
 
 ObjString *copyString(const char *chars, int length) {
     uint32_t hash = hashString(chars, length);
-    ObjString* interned = tableFindString(&vm.strings, chars, length,
-                                        hash);
+    ObjString *interned = tableFindString(&vm.strings, chars, length,
+                                          hash);
     if (interned != NULL) return interned;
 
     char *heapChars = ALLOCATE(char, length + 1);
@@ -61,8 +75,22 @@ ObjString *copyString(const char *chars, int length) {
     return allocateString(heapChars, length, hash);
 }
 
+static void printFunction(ObjFunction *function) {
+    if (function->name == NULL) {
+        printf("<script>");
+        return;
+    }
+    printf("<fn %s>", function->name->chars);
+}
+
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_FUNCTION:
+            printFunction(AS_FUNCTION(value));
+            break;
+        case OBJ_NATIVE:
+            printf("<native fn>");
+            break;
         case OBJ_STRING:
             printf("%s", AS_CSTRING(value));
             break;
