a52482e26dbbfd507afbf5669215faa110136c55
finallie <1006492957@qq.com>
2024-08-27 15:21:03 +0800

    gc

--- compiler.c
+++ compiler.c
@@ -11,6 +11,7 @@
 #endif
 #include <string.h>
 
+#include "memory.h"
 #include "scanner.h"
 
 typedef struct {
@@ -816,4 +817,12 @@ ObjFunction *compile(const char *source) {
 
     ObjFunction *function = endCompiler();
     return parser.hadError ? NULL : function;
+}
+
+void markCompilerRoots() {
+    Compiler* compiler = current;
+    while (compiler != NULL) {
+        markObject((Obj*)compiler->function);
+        compiler = compiler->enclosing;
+    }
 }
