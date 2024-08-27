3694831e5377fd17cbb6963a9d255d8c21300fab
finallie <1006492957@qq.com>
2024-08-27 15:56:50 +0800

    class

--- object.c
+++ object.c
@@ -47,9 +47,15 @@ static uint32_t hashString(const char *key, int length) {
     return hash;
 }
 
+ObjClass *newClass(ObjString *name) {
+    ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+    klass->name = name;
+    return klass;
+}
+
 ObjClosure *newClosure(ObjFunction *function) {
-    ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*,
-                                   function->upvalueCount);
+    ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue*,
+                                     function->upvalueCount);
     for (int i = 0; i < function->upvalueCount; i++) {
         upvalues[i] = NULL;
     }
@@ -118,6 +124,9 @@ static void printFunction(ObjFunction *function) {
 
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_CLASS:
+            printf("%s", AS_CLASS(value)->name->chars);
+            break;
         case OBJ_CLOSURE:
             printFunction(AS_CLOSURE(value)->function);
             break;
